9b62a5e7afc476bb8612d0078345ecbc2ad641ed
unknown <you@example.com>
2018-04-21 13:18:59 -0500

    practica proyectos

---
+++ William/practica_en_equipo_AyW.py
@@ -0,0 +1,40 @@
+r= 1.1
+pi= 3.1416
+area= pi*r*r
+print (area)
+
+n= 21-1
+print(n)
+
+subtotal= 44.50
+propina= .10*44.50
+total= subtotal+propina
+print(total)
+
+gravedad_en_marte= 3.72
+gravedad_tierra= 9.78
+peso= 70
+peso_tierra= peso/gravedad_tierra
+peso_en_marte= peso_tierra*gravedad_en_marte
+print(peso_en_marte)
+
+print("adrian"*100)
+
+
+letras= ["a","b","c","d","e","f","g","h","i","j"]
+print (letras [7])
+
+print("adrian".upper())
+
+print("WILLIAM".lower())
+
+print("hello worl"[0])
+print("hello worl"[9])
+
+nombre= "william"
+apellido="hernandez"
+print(apellido, nombre)
+
+
+
+
