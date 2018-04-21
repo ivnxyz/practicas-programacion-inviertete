ccbb527ea9b5e340dd7a4e534383dd6e574766e4
Iván Martínez <ivanmartinez33@outlook.com>
2018-04-21 13:01:22 -0500

    proyecto numeros incompleto

--- RENE_RMZ/TRABAJO1.py
+++ RENE_RMZ/TRABAJO1.py
@@ -4,6 +4,40 @@
 radio= 6
 area= (radio*radio)*3.1416
 print("area del circulo es igual a", area)
+print()
+print()
 
 #2.-Devuelve el valor absoluto
-#Dado un entero n, devuelve el valor absoluto de la diferencia entre n y 21
+#Dado un entero n, devuelve el valor absoluto de la diferencia entre n y 21.
+
+n=1
+valor=21
+diferencia= n-valor
+print("la diferencia entre", n,"y 21 es", diferencia)
+print()
+print()
+
+#3.-Calculadora de propinas
+#Crea un pequeño programa que calcule la propina.(propina 10%)
+precio=357
+propina=precio*.10
+total=102
+print("Por la cantidad de",precio,"la propina sera de",propina)
+print()
+print()
+
+#4.-Calcula tu peso en otro planeta
+#Crea un programa que calcule tu peso en otro planeta.
+#Usa variables para contener tu peso, la gravedad de la tiera y la gravedad del planeta en el que quieres calcular tu peso.
+masa= 76
+gravedadDeLaTierra=9.78
+gravedadDeJupiter=22.88
+PesoEnLaTierra=masa*gravedadDeLaTierra
+PesoEnJupiter=masa*gravedadDeJupiter
+print("El peso en la tierra es igual",PesoEnLaTierra)
+print()
+print("El peso en jupiter es igual a",PesoEnJupiter)
+print()
+print()
+
+#
