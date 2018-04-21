b9a275e58b83c9b7bd47f738206dcea3bd8b251f
unknown <you@example.com>
2018-04-21 12:36:53 -0500

    ejercicio numeros

---
+++ Frida/ejercicios.py
@@ -0,0 +1,18 @@
+#Ejercicio 1
+r=(1.1)*(1.1)
+area=r*3.1416
+print(area)
+#Ejercicio 2 
+n=1
+diferencia=21-n
+print(diferencia)
+#Ejercicio 3
+subtotal=44.50
+propina=subtotal*.10
+total=subtotal+propina
+print(total)
+#Ejercicio 4
+gravedad_marte=3.72
+peso=26.3
+masa=peso/gravedad_marte
+print(masa*gravedad_marte)
