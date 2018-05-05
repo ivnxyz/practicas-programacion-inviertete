c6edfb0f9c2c7b68d252699a0ab183dc1ea17106
unknown <you@example.com>
2018-05-05 12:45:46 -0500

    encontre cuantas veces se repite una letra

--- jose Manuel/formar la frase con diferentes string.py
+++ jose Manuel/formar la frase con diferentes string.py
@@ -1,4 +1,4 @@
 str1 = "jfjdHvbcxyz"
 str2 = "ofjvcv!ldba"
 str3 = "mgbunfbd "
-print(str1[4]+str2[0]+str2[7]+str2[10], str3[0]+str3[3]+str3[4]+str1[3]+str2[0]+str2[6])
+print(str1[4]+str2[0]+str2[7]+str2[10],str3[0]+str3[3]+str3[4]+str1[3]+str2[0]+str2[6])
