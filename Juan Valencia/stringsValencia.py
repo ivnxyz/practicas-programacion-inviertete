abbcb56ea1c5bfe2f4eca2847511cc01ceaee1a8
unknown <you@example.com>
2018-05-05 11:43:19 -0500

    se avanzó

--- Juan Valencia/stringsValencia.py
+++ Juan Valencia/stringsValencia.py
@@ -37,4 +37,22 @@ str2 = "ofjvcv!ldba"
 str3 = "mgbunfbd "
 print(str1[4],str2[0],str2[7],str2[10],"","",str3[0],str3[3],str3[4],str3[7],str2[0],str2[6])
 
+#9. Encuentra cuántas veces aparece la letra 'a' en la siguiente String.
+lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium nisl nec ullamcorper dignissim. Aliquam facilisis efficitur turpis at sagittis. Suspendisse sed tristique augue, luctus sagittis urna. Ut nec porttitor tellus, vel feugiat ipsum. Nam maximus faucibus ipsum id ullamcorper. Duis leo ligula, congue sed blandit a, dictum nec metus. Duis lobortis libero eu convallis placerat. Donec felis lacus, faucibus sed massa non, tincidunt hendrerit odio. Ut efficitur sapien in nibh elementum bibendum. Phasellus vulputate tempor pretium. Proin viverra ante eu mi commodo rhoncus. Nullam vulputate tempus gravida. Fusce auctor massa eu suscipit porta. Aenean non elit convallis lacus eleifend lacinia non ac tortor. Nam efficitur dolor ac dolor fringilla venenatis. Fusce nec eros dictum, congue massa a, ornare nibh. Sed id tristique magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam sit amet ligula ac dui viverra vestibulum. Nunc faucibus cursus felis, et lacinia ex lobortis non. Vestibulum lobortis vel ligula vitae mattis. In sed velit sed purus imperdiet elementum et et tellus. Morbi ut metus sed urna dictum porttitor nec non tortor. Proin luctus massa laoreet lectus lacinia congue. Donec consectetur sollicitudin sollicitudin. Sed nec efficitur eros, eget semper ligula. Suspendisse sodales risus ex, id blandit elit tincidunt id. Proin vestibulum posuere enim, sed molestie nibh dapibus sed. Curabitur feugiat nibh non leo eleifend, non tempor lorem semper. Curabitur auctor ligula id velit lacinia, sit amet blandit augue eleifend. Donec ut tortor dui. Etiam maximus velit sed aliquet pretium. Suspendisse purus nibh, ultricies nec velit in, malesuada scelerisque est. Etiam pharetra dolor facilisis, porttitor felis sit amet, lobortis orci. Quisque massa elit, aliquet et aliquam non, egestas quis neque. Sed pulvinar lacus in tellus pretium, sit amet vestibulum felis malesuada. Proin porta libero mauris, id porttitor velit consequat sed. Suspendisse potenti. Suspendisse dapibus ligula non nibh mattis vestibulum. Aliquam erat volutpat. Aliquam rutrum ex dictum nunc ullamcorper, ac mollis tortor feugiat. Aenean a nunc bibendum, porttitor dolor in, pulvinar lorem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras tempus elementum ipsum, vitae porta metus elementum quis. Proin et convallis risus. Vestibulum fringilla lectus id velit mattis sodales. Donec viverra ligula id malesuada commodo. Pellentesque ac dignissim mauris. Curabitur ut nunc vitae dolor pharetra iaculis in sit amet justo."
+lorem_ipsum.count("a")
+contar=lorem_ipsum.count("a")
+print(contar)
+
+#10.Separa la siguiente String por comas.
+lista_de_compras = "Manzanas,zanahorias,cereal,leche,ramen"
+print(lista_de_compras.split(","))
+
+#11.Imprime tu nombre completo con un espacio.
+apellido="Valencia Toledo"
+nombres="Juan Emmanuel"
+print(apellido, " ", nombres)
+
+#12.Imprime la siguientes Strings separadas por comas.
+
+
 
