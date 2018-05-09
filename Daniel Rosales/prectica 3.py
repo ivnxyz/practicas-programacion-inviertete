3b1a4ee088ce048b6ac50afdd7c7efcafef89067
unknown <you@example.com>
2018-05-09 11:32:42 -0500

    Práctica 3 terminada

---
+++ Daniel Rosales/prectica 3.py
@@ -0,0 +1,9 @@
+print("***BIENVENIDO AL ADIVINADOR***")
+x= int(input("Por favor indica el limite superior : "))
+print("Jugando con numeros entre 1 y", (x))
+print("*************************************")
+import random
+x=random.randint(1,100)
+print("dame un numero entre 1 y ",(x),":")
+
+
