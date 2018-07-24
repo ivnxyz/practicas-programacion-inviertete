d3515a442161d534654bcfd649cb5477e9885685
unknown <you@example.com>
2018-07-24 14:21:12 -0500

    proyecto alfa finalizado

--- DanielAragón/Proyecto alfa/Proycto alfa.py
+++ DanielAragón/Proyecto alfa/Proycto alfa.py
@@ -11,6 +11,7 @@ with open("usuarios.txt") as usuarios:
     edades=[]
     print("intento 0 de 3")
     u=input("Usuario: ")
+    c=input("Contraseña: ")
     contador=1
     
 for usuario in usuarios:
@@ -31,35 +32,43 @@ for c2 in contra:
     lista2.append(c2)
     contador += 1
 
+
+
+
 intentos = 0
+cine=1
+
 while intentos < 3: 
     if u in lista:
-        c=input("Contraseña: ")
         valor=lista.index(u)
         if c == lista2[valor]:
-            with open("peliculas.txt") as peliculas:
-                peliculas = peliculas.readlines()
-                peli=1
-            for pelicula in peliculas:
-                cartelera[peli] = pelicula
-                print(peli,'.',pelicula)
-                peli+=1
-            cine=input("¿Qué pelicula quieres ver?")
-            boletos=int(input("¿Cuantos boletos quiere comprar?"))
-            contador = 0
-            while boletos > contador:
-                edades.append((int(input("Ingresa las edades: "))))
-                if edades[contador]<=12:
-                    edades[contador]=50
-                    contador+=1
+            while cine!=0:
+                with open("peliculas.txt") as peliculas:
+                    peliculas = peliculas.readlines()
+                    peli=0
+                for pelicula in peliculas:
+                    cartelera[peli] = pelicula
+                    print(peli,'.',pelicula)
+                    peli+=1
+                cine=int(input("¿Qué pelicula quieres ver?"))
+                if cine == 0:
+                    exit()
                 else:
-                    edades[contador]=100
-                    contador+=1
-            i=0
-            for i in edades:
-               i=i+i
-            print(i)
-                
+                    boletos=int(input("¿Cuantos boletos quiere comprar?"))
+                    contador = 0
+                while boletos > contador:
+                    edades.append(int(input("Ingresa las edades: ")))
+                    if edades[contador]<13:
+                        edades[contador]=50
+                        contador+=1
+                    else:
+                        edades[contador]=100
+                        contador+=1
+                i=0
+                for a in edades:
+                   i=i+a
+                print("El total es de $", i ," para la pelicula", cine)
+                edades=[]
         else:
             intentos += 1
             print("Contraseña equivocada")
@@ -69,9 +78,11 @@ while intentos < 3:
         print ("intento", intentos,"de 3")
         if intentos < 3:
             u=input("Usuario: ")
+            c=input("Contraseña: ")
         else:
             exit()
         
+exit()
 
         
 
--- DanielAragón/Proyecto alfa/proyecto alfa B.py
+++ DanielAragón/Proyecto alfa/proyecto alfa B.py
@@ -5,39 +5,92 @@ with open("usuarios.txt") as usuarios:
     usuarios = usuarios.readlines()
     diccionarioA = {}
     diccionarioB = {}
-    print("intento 0 de 3")
-    u=input("Usuario: ")
+    cartelera = {}
+    lista=[]
+    lista2=[]
+    edades=[]
+    print("intento 1 de 3")
     contador=1
     
 for usuario in usuarios:
     diccionarioA[contador]=usuario.split('|')[1].strip()
     diccionarioB[contador]=usuario.split('|')[0]
     contador+=1
-    
+
 usu=diccionarioB.values()
-contra=diccionarioA.values()   
-intentos = 1
+contra=diccionarioA.values()
+
+
+contador = 0
 for u2 in usu:
-    if  u == u2:
-        c=input("Contraseña: ")
-        for c2 in contra:
-            if c == c2:
-                with open("peliculas.txt") as peliculas:
-                    peliculas = peliculas.readlines()
-                    peli=1
-                for pelicula in peliculas:
-                    print(peli,'.',pelicula)
-                    peli+=1
-                exit()
-            elif intentos > 4:
+    lista.append(u2)
+    contador += 1
+contador = 0
+for c2 in contra:
+    lista2.append(c2)
+    contador += 1
+
+intentos = 1
+while intentos <=3:
+    u=input("Usuario: ")
+    c=input("Contraseña: ")
+    if u in lista:
+        valor=lista.index(u)
+        if c == lista2[valor]:
+            with open("peliculas.txt") as peliculas:
+                peliculas = peliculas.readlines()
+                peli=0
+                intentos=4
+        else:
+            intentos += 1
+            if intentos == 4:
                 exit()
             else:
-                intentos +=1
+                print("Contraseña equivocada")
+                print ("intento", intentos,"de 3")
+    else:
+        intentos += 1
+        if intentos == 4:
+            exit()
+        else:
+            print("Usuario inexistente: ")
+            print ("intento", intentos,"de 3")
+            
+cine=1
+while cine!=0:  
+    for pelicula in peliculas:
+        cartelera[peli] = pelicula
+        print(peli,'.',pelicula)
+        peli+=1
+    cine=int(input("¿Qué pelicula quieres ver? (0 para salir)"))
+    if cine == 0:
+        exit()
+    else:
+        boletos=int(input("¿Cuantos boletos quiere comprar?"))
+        contador = 0
+    while boletos > contador:
+        edades.append((int(input("Ingresa las edades: "))))
+        if edades[contador]<13:
+            edades[contador]=50
+            print(edades[contador])
+            contador+=1
+        else:
+            edades[contador]=100
+            print(edades[contador])
+            contador+=1
+            
+    total=0
+    for i in edades:
+       total=total+i
+    print("El total es de $", total ," para la pelicula", cine)
+    edades=[]
                 
-print("Usuario inexistente")
+           
+        
 exit()
-##            else:
-##                print("Usuario o contraseña erronea")
-##                print("intento ", intentos,"de 3")
-##                intentos +=1
-##                    
+
+        
+
+
+           
+    
